cc8ae49b21fd46d48d0430259f825b960bdef5b3
Mark Nuttall <mnuttall@uk.ibm.com>
2016-06-24 11:57:40 +0100

    Initial commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+class Test { 
+
+public static void main (String argv[]) { 
+System.out.println ("Hello world");
+}
+
+}
---
+++ src/main/java/test/TestServlet.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/")
+public class TestServlet extends HttpServlet { 
+  
+  public void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException { 
+    resp.getWriter().println ("Hello"); 
+  }
+
+}
