17c58036fd1553c0ef78c73ed99280d444a89f1e
Mark Nuttall <mnuttall@uk.ibm.com>
2016-06-27 10:09:09 +0100

    Add a trivial unit test

---
+++ src/test/java/sample/HelloTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.junit.Test;
+
+public class HelloTest { 
+
+  @Test
+  public void testSomething() { 
+    // We should get a test pass even without any assertions
+  }
+
+}
